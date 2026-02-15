ea305496c9984d63a007fd0304b23e508f0e24d6
Eranda <y.eranda@htmail.com>
2026-02-16 00:07:45 +0530

    Mintpay added both run parralel

--- scrape.js
+++ scrape.js
@@ -11,61 +11,96 @@ const __dirname = dirname(__filename);
 
 const logging = logger(__filename);
 
-async function appProcess() {
+async function appProcess(platforms) {
   logging.info("Starting the bot...");
 
   let web;
-  let extractionStatus = false;
 
   try {
     web = new WebHandler();
     await web.init();
-
-    try {
-      // Attempt Login
-      const loginSuccess = await web.kokoLogin(
-        process.env.KOKO_USERNAME,
-        process.env.KOKO_PASSWORD
-      );
-
-      logging.info(`Login Result ${loginSuccess}`);
-
-      if (loginSuccess) {
-        logging.info("LOGIN SUCCESS");
-        try {
-          // Attempt Extraction
-          const dataExtractionStatus = await web.kokoExtractProcess();
-
-          if (dataExtractionStatus) {
-            logging.info("Data Extraction Complete");
-            web.destroy();
-            extractionStatus = true;
-          }
-        } catch (e) {
-          logging.critical(`KOKO extraction error: ${e.message}`);
-        }
-      }
-    } catch (e) {
-      logging.critical(`KOKO Unknown Error during flow: ${e.message}`);
-    }
   } catch (e) {
     logging.critical(`Web browser load failed: ${e.message}`);
   }
 
-  if (extractionStatus) {
-    // console.log("DONE DONE DONE");
-    if (uploadToGoogleSheet("koko.json", process.env.DATA_SUBMIT_ENDPOIT))
-      logging.info("-----| DONE |--------");
+  for (const requestedPlatfrom of platforms) {
+    let loginStatus = false;
+    let dataExtractionStatus = false;
+    let uploadStatus = false;
+
+    try {
+      switch (requestedPlatfrom) {
+        case "KOKO":
+          loginStatus = await web.kokoLogin(
+            requestedPlatfrom,
+            process.env.KOKO_USERNAME,
+            process.env.KOKO_PASSWORD,
+          );
+          break;
+        case "MINT":
+          loginStatus = await web.mintPayLogin(
+            requestedPlatfrom,
+            process.env.MINTPAY_USERNAME,
+            process.env.MINTPAY_PASSWORD,
+          );
+          break;
+      }
+    } catch (e) {
+      logging.critical(`${requestedPlatfrom} Unknown Error: ${e}`);
+    }
+
+    if (loginStatus) {
+      try {
+        switch (requestedPlatfrom) {
+          case "KOKO":
+            dataExtractionStatus =
+              await web.kokoExtractProcess(requestedPlatfrom);
+            break;
+          case "MINT":
+            dataExtractionStatus =
+              await web.mintPayExtractProcess(requestedPlatfrom);
+            break;
+        }
+      } catch (e) {
+        logging.critical(`${requestedPlatfrom} Unknown Error: ${e}`);
+      }
+    } else {
+      /*  TODO - HANDLE LOGIN FAILED */
+    }
+
+    if (dataExtractionStatus) {
+      logging.info(
+        `${requestedPlatfrom} data succefully extracted & saved in to ${requestedPlatfrom}.json!`,
+      );
+
+      if (requestedPlatfrom === "KOKO") {
+        uploadStatus = uploadToGoogleSheet(
+          requestedPlatfrom,
+          `${requestedPlatfrom}.json`,
+          process.env.DATA_SUBMIT_ENDPOIT,
+        );
+      }
+
+      if (uploadStatus) {
+        logging.info(`Bot ran successfully - ${requestedPlatfrom}`);
+      }
+    } else {
+      /*  TODO - HANDLE DATA EXTRACTION FAILED */
+    }
   }
+
+  web.destroy();
 }
 
 (async () => {
   logging.info("----------| NEW PROCESS START |----------");
   try {
-    await appProcess();
+    const pltforms = ["KOKO", "MINT"];
+    // const pltforms = ["KOKO"];
+    // const pltforms = ["MINT"];
+    await appProcess(pltforms);
+    logging.info("**********| END |**********");
   } catch (e) {
     logging.critical(`Critical System Error: ${e.message}`);
   }
-
-  logging.info("**********| END |**********");
 })();
